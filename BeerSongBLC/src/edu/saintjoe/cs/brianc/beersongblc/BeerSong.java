a55f2241abaa1b5a320ee5282c56510e8056a26a
Brian Capouch <brianc@palaver.net>
2016-02-12 14:25:23 -0500

    Fix the bottles bug

--- BeerSongBLC/src/edu/saintjoe/cs/brianc/beersongblc/BeerSong.java
+++ BeerSongBLC/src/edu/saintjoe/cs/brianc/beersongblc/BeerSong.java
@@ -4,16 +4,18 @@ public class BeerSong {
     public static void main(String[] args) {
         int beerNum = 99;
         String word = "bottles";
-        while (beerNum > 0)  {
-            if (beerNum == 1) {
-              word = "bottle";
-              } // endif 
+        while (beerNum > 0)  { // While there's bottles 
             System.out.println(beerNum + " " + word + " of beer on the wall");
             System.out.println(beerNum + " " + word + " of beer");
             System.out.println("Take one down.");
             System.out.println("Pass it around.");
             beerNum = beerNum - 1;
-            if (beerNum > 0) {
+            if (beerNum == 1) { // Correct for one bottle
+                word = "bottle";
+                } // endif
+            
+            // Adjust for case of NO bottles
+            if (beerNum > 0) { 
                 System.out.println(beerNum +  " " + word + " of beer on the wall");
             } else {
                 System.out.println("No more bottles of beer on the wall");
